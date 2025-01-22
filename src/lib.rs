d39e48d75e97eaede8329cf26f9135b75542bf4d
Shreyash <hello@webdevcaptain.com>
2025-01-22 13:36:36 +0000

    feat(watcher): put a basic implementation using notify
    use watcher from notify and async queues from tokio for initial version

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,12 @@
 //!
 //! A simple file watcher that watches a list of paths and calls a callback when any of them change
 
+use anyhow::Result;
+use notify::{self, Event, RecommendedWatcher, RecursiveMode, Watcher};
 use std::path::PathBuf;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
+use tokio::sync::mpsc;
 
 /// A simple file watcher for monitoring file-system events (changes)
 pub struct FileWatcher {
@@ -16,10 +21,33 @@ impl FileWatcher {
     }
 
     /// Starts watching the provided paths and triggers the callback when any of them change
-    pub async fn watch<F>(self, callback: F)
+    pub async fn watch<F>(self, mut callback: F) -> Result<(), notify::Error>
     where
-        F: FnMut(),
+        F: FnMut(Event) + Send + 'static,
     {
-        todo!()
+        let (tx, mut rx) = mpsc::channel(100);
+
+        thread::spawn(move || {
+            let (raw_tx, raw_rx): (Sender<Result<Event, notify::Error>>, Receiver<_>) = channel();
+
+            let mut watcher: RecommendedWatcher =
+                Watcher::new(raw_tx, notify::Config::default()).unwrap();
+
+            for path in self.paths {
+                watcher.watch(&path, RecursiveMode::Recursive).unwrap();
+            }
+
+            while let Ok(event) = raw_rx.recv() {
+                if let Ok(event) = event {
+                    let _ = tx.blocking_send(event);
+                }
+            }
+        });
+
+        while let Some(event) = rx.recv().await {
+            callback(event);
+        }
+
+        Ok(())
     }
 }
